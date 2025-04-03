a8ad74b39a8caa05a89616cdae954ac8006d3f15
Edward Ruzek <edadiddles@hotmail.com>
2025-04-03 08:50:21 -0400

    adding some dirty parsing of the request and responding with 200 and 404 status codes

--- app/server.go
+++ app/server.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net"
 	"os"
+    "bytes"
 )
 
 func main() {
@@ -22,5 +23,28 @@ func main() {
 		os.Exit(1)
 	}
 
-    conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+    req := make([]byte, 100)
+    conn.Read(req)
+
+    p_req := make([][][]byte, 0)
+    vals := make([][]byte, 0)
+    beg_idx := 0
+    for i:=0; i < len(req); i++ {
+        if req[i] == ' ' {
+            vals = append(vals, req[beg_idx:i])
+            beg_idx = i+1
+        } else if req[i] == '\r' && len(req) >= i+1 && req[i+1] == '\n' {
+            vals = append(vals, req[beg_idx:i])
+            p_req = append(p_req, vals)
+            vals = make([][]byte, 0)
+            i++ // catch up to line feed
+            beg_idx = i+1
+        }
+    }
+
+    if bytes.Equal(p_req[0][1], []byte("/")) {
+        conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+    } else {
+        conn.Write([]byte("HTTP/1.1 404 Not Found \r\n\r\n"))
+    }
 }
