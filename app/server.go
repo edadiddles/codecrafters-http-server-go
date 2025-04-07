be8b869f307aaaf0034b82f83ec5f9781356238d
Edward Ruzek <edadiddles@hotmail.com>
2025-04-07 09:21:46 -0400

    writing body to file

--- app/server.go
+++ app/server.go
@@ -6,6 +6,7 @@ import (
 	"os"
     "io"
     "bytes"
+    "strconv"
 )
 
 func main() {
@@ -42,20 +43,38 @@ func handleRequest(conn net.Conn, dir string) {
     p_req := make([][][]byte, 0)
     vals := make([][]byte, 0)
     beg_idx := 0
+    is_content_length := false
+    content_length := 0
     for i:=0; i < len(req); i++ {
         if req[i] == '\x00' {
             vals = append(vals, req[beg_idx:i])
             p_req = append(p_req, vals)
             break
         } else if req[i] == ' ' {
+            if bytes.Equal(req[beg_idx:i], []byte("Content-Length:")) {
+                is_content_length = true
+            }
             vals = append(vals, req[beg_idx:i])
             beg_idx = i+1
         } else if req[i] == '\r' && len(req) >= i+1 && req[i+1] == '\n' {
+            if is_content_length {
+                is_content_length = false
+                content_length, _ = strconv.Atoi(string(req[beg_idx:i]))
+            }
             vals = append(vals, req[beg_idx:i])
             p_req = append(p_req, vals)
             vals = make([][]byte, 0)
             i++ // catch up to line feed
             beg_idx = i+1
+            if req[i+1] == '\r' && len(req) >= i+2 && req[i+2] == '\n' {
+                i++
+                i++
+                beg_idx = i+1
+                vals = append(vals, req[beg_idx:beg_idx+content_length])
+                p_req = append(p_req, vals)
+                beg_idx += content_length
+                break
+            }
         }
     }
 
@@ -76,7 +95,7 @@ func handleRequest(conn net.Conn, dir string) {
         }
     } else if bytes.Equal(p_req[0][1][0:7], []byte("/files/")) {
         filename := string(p_req[0][1][7:])
-        if bytes.Equal(p_req[0][1], []byte("GET")) {
+        if bytes.Equal(p_req[0][0], []byte("GET")) {
             f, err := os.Open(dir+filename)
             if err != nil {
                 conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
@@ -94,10 +113,10 @@ func handleRequest(conn net.Conn, dir string) {
                 out := fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: %d\r\n\r\n%s", len(content), content)
                 conn.Write([]byte(out))
             }
-        } else if bytes.Equal(p_req[0][1], []byte("POST")) {
+        } else if bytes.Equal(p_req[0][0], []byte("POST")) {
             content := p_req[len(p_req)-1][0]
             os.WriteFile(dir+filename, content, 0666)
-            conn.Write([]byte("HTTP/1.1 201 Created\r\n"))
+            conn.Write([]byte("HTTP/1.1 201 Created\r\n\r\n"))
         }
     } else {
         conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
